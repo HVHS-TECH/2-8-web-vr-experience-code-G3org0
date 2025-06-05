60b2041f3141b17463a5bb4337faf867f3fe5aea
G3org0 <22123gt@hvhs.school.nz>
2025-06-05 03:14:10 +0000

    Adding Js

--- game/game.js
+++ game/game.js
@@ -1,11 +1,18 @@
 
 let wallCords = 6
 
+let scale = {
+  xMult: 1, 
+  yMult: 0.9090900177208052, 
+  zMult: 0.9999998437456502,
+  xPlac: 12,
+  yPlac: 2}
 
 const wallPlacment = ([
-    [1, 2], 
+    [1, 2, 3, 4, 5, 6, 7, 8, 9, 10, ], 
     [3, 4], 
-    [5, 6]
+    [5, 6], 
+    [1, 2, 3, 4, 5, 6, 7, 8, 9, 10, ]
 ]);
 
 console.log(wallPlacment.length)
@@ -19,7 +26,13 @@ window.addEventListener('DOMContentLoaded', () => {
 function createWalls(){
     for (let row = 0; row < wallPlacment.length; row++){
         for (let coloumn = 0; coloumn < wallPlacment[row].length; coloumn++ ){
-            createWall(coloumn, row)
+            let rot = false;
+            if ((row/2) % 1 == 0.5){
+                rot = true;
+            }
+            
+            //console.log(wallPlacment[row][coloumn])
+            createWall(wallPlacment[row][coloumn], row)
             //console.log(row)
            // console.log(wallPlacment[row]);
         }
@@ -28,16 +41,22 @@ function createWalls(){
 }
 
     
-function createWall(xCord, yCord){
-    console.log("runnin createWall");
+function createWall(xCord, zCord){
+    //console.log("runnin createWall");
     const WALL = document.createElement('a-gltf-model');
     WALL.setAttribute('src','#wall1');
-    WALL.setAttribute('position', `${xCord} 0 ${yCord}`);
-    console.log(WALL);
+    WALL.setAttribute('position', `${xCord*scale.xPlac} 0 ${zCord*scale.xPlac}`);
+    WALL.setAttribute('scale', `${scale.xMult} ${scale.yMult} ${scale.zMult}`);
     SCENE.appendChild(WALL);
 }
 
-//const model = document.querySelector('#wall1');
+
+
+
+
+
+
+//const model = document.querySelector('#wallingAway');
 //
 //model.addEventListener('model-loaded', () => {
 //  const mesh = model.getObject3D('mesh');
@@ -48,4 +67,6 @@ function createWall(xCord, yCord){
 //  box.getSize(size);
 //
 //  console.log('Model size:', size); // size.x, size.y, size.z
+//  console.log(12 / size.x)
+//  console.log(2 / size.y)
 //});
