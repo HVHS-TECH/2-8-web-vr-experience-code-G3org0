e02009c8fae6c7cb6afd7cba64c766040ea40419
G3org0 <22123gt@hvhs.school.nz>
2025-06-11 02:44:58 +0000

    adding frogs

--- game/game.js
+++ game/game.js
@@ -30,7 +30,18 @@ let globalShift = {
      [1, 1, 1, 1, 1, 1, 1, 1, 1, 1], 
 ]);
 
+const FrogPlacment = ([
+     [1,1,0], 
+     [1,4,0], 
+     [2.5,9,0], 
+     [4,3,0],
+     [4.5,2.5,180],
+     [4.5,6,0],
+
+]);
+
 const SCENE = document.querySelector('a-scene');
+const BOX = document.querySelector('#detectionBox');
 
 window.addEventListener('DOMContentLoaded', () => {
     createWalls(); 
@@ -51,22 +62,7 @@ function createWalls(){
         }
     }
 }
-
-function createPillers(){
-    for (let row = 0; row < (wallPlacment.length + 1)/2; row++){
-        console.log("Max Row :  " + (wallPlacment.length + 1)/2)
-        for (let coloumn = 0; coloumn < wallPlacment[0].length + 1; coloumn++ ){
-            createPiller(coloumn, row);
-        }
-    }
-}
-
-function createFrogs(){
-    
-}
-
 function createWall(xCord, zCord, rotationVal){
-    //console.log("runnin createWall");
     const WALL = document.createElement('a-gltf-model');
     if (rotationVal == 0){
         WALL.setAttribute('position', `${xCord*scale.xPlac-globalShift.x} ${globalShift.y} ${zCord*scale.xPlac*0.5-globalShift.z}`);
@@ -79,18 +75,39 @@ function createWall(xCord, zCord, rotationVal){
     SCENE.appendChild(WALL);
 }
 
+function createPillers(){
+    for (let row = 0; row < (wallPlacment.length + 1)/2; row++){
+        console.log("Max Row :  " + (wallPlacment.length + 1)/2)
+        for (let coloumn = 0; coloumn < wallPlacment[0].length + 1; coloumn++ ){
+            createPiller(coloumn, row);
+        }
+    }
+}
 function createPiller(xCord, zCord){
-    console.log("Y 5 THO")
-    //console.log(xCord, zCord);
     const PILLER = document.createElement('a-gltf-model');
-    PILLER.setAttribute('position', `${(xCord - 0.5)*scale.xPlac-globalShift.x} globalShift.y ${zCord*scale.xPlac-globalShift.z}`);
+    PILLER.setAttribute('position', `${(xCord - 0.5)*scale.xPlac-globalShift.x} ${globalShift.y} ${zCord*scale.xPlac-globalShift.z}`);
     PILLER.setAttribute('src','#piller');
     PILLER.setAttribute('scale', `${scale.xMult} ${scale.yMult} ${scale.zMult}`);
     SCENE.appendChild(PILLER);
 }
 
+function createFrogs(){
+    for (let i = 0; i <FrogPlacment.length; i++){
+        createFrog(FrogPlacment[i][1], FrogPlacment[i][0], FrogPlacment[i][2]-180)
+    }
+}
+function createFrog(xCord, zCord, rotationVal){
+    const FROG = document.createElement('a-gltf-model');
+    FROG.setAttribute('position', `${(xCord - 0.5)*scale.xPlac-globalShift.x} ${0} ${zCord*scale.xPlac-globalShift.z}`);
+    FROG.setAttribute('src','#frog');
+    FROG.setAttribute('rotation', `0 ${rotationVal}  0`);
+    FROG.setAttribute('scale','1 1 1');
+    SCENE.appendChild(FROG);
+}
 
-
+if (BOX.x == null){
+    console.log("aaa")
+}
 
 //const model = document.querySelector('#wallingAway');
 //
